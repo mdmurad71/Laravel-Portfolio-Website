e07c4e2af6d72e30e6f161383568d4fa0eef0d34
mdmurad71 <63662868+mdmurad71@users.noreply.github.com>
2020-05-16 14:33:39 +0600

    Update web.php

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+
+Route::get('/', 'HomeController@HomeIndex');
+Route::get('/visitor', 'VisitorController@VisitorIndex');
+Route::get('/services', 'ServicesController@ServiceIndex');
+Route::get('/Courses', 'CourseController@CourseIndex');
+
+
+
+//service route portion
+Route::get('/getServicesData', 'ServicesController@getServicesData');
+Route::post('/ServiceDelete', 'ServicesController@serviceDelete');
+Route::post('/serviceUpdate', 'ServicesController@ServiceUpdate');
+Route::post('/updatefinal', 'ServicesController@updateFinal');
+Route::post('/addfinal', 'ServicesController@addFinal');
+
+//home route portion
+Route::get('/getHomeWorkData', 'HomeController@getHomeWorkData');
+Route::post('/homeDelete', 'HomeController@homeDelete');
+Route::post('/homeUpdate', 'HomeController@homeUpdate');
+Route::post('/homeUpdateFinal', 'HomeController@homeUpdateFinal');
+
+
+//courses route portion
+Route::get('/getCoursesData', 'CourseController@getCoursesData');
+Route::post('/CourseDelete', 'CourseController@CourseDelete');
+Route::post('/CourseDeatails', 'CourseController@CourseDeatails');
+Route::post('/Courseupdate', 'CourseController@Courseupdate');
+Route::post('/CourseaddFinal', 'CourseController@CourseaddFinal');
